ad15fbdb7b131b7a1dca8a5007f230d79118a272
Adeleke Michael Olamide <adelekem600@gmail.com>
2026-01-09 14:59:58 +0100

    Change LoadingSpinner to named export

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -7,9 +7,8 @@ interface LoadingSpinnerProps {
   color?: string;
 }
 
-export default function LoadingSpinner({ size = 40, color = colors.primary }: LoadingSpinnerProps) {
+export function LoadingSpinner({ size = 40, color = colors.primary }: LoadingSpinnerProps) {
   const rotation = useRef(new Animated.Value(0)).current;
-
   const spin = rotation.interpolate({
     inputRange: [0, 1],
     outputRange: ['0deg', '360deg'],
@@ -29,9 +28,7 @@ export default function LoadingSpinner({ size = 40, color = colors.primary }: Lo
       animation.stop();
       rotation.setValue(0);
     };
-    // rotation is stable (ref), safe to leave deps empty
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [rotation]);
 
   return (
     <View style={styles.container}>
